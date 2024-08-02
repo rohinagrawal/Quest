987d7fba660e17802d43fe44d664a55bd1847a4d
Rohin Agrawal <rohin.k.agrawal@gmail.com>
2024-08-02 15:31:58 +0530

    search_sorted_matrix solution added

--- src/main/java/array/search_sorted_matrix/Code.java
+++ src/main/java/array/search_sorted_matrix/Code.java
@@ -5,10 +5,18 @@ import java.util.ArrayList;
 public class Code {
     public int solve(ArrayList<ArrayList<Integer>> A, int B) {
         int i= 0;
-        int j= A.size();
-        while (i<A.size()){
-
+        int j= A.get(i).size()-1;
+        while (i>=0 && i<A.size() && j>=0 && j< A.get(i).size()){
+            if (A.get(i).get(j) == B && (j<=0 || A.get(i).get(j-1) < B)){
+                return (i+1) * 1009 + (j+1);
+            }
+            else if (A.get(i).get(j) >= B){
+                j--;
+            }
+            else {
+                i++;
+            }
         }
-        return i;
+        return -1;
     }
 }
