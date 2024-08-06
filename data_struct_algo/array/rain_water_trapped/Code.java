55875b6ca35636e5620fdf60f296d4d7c0e1039d
Rohin Agrawal <rohin.k.agrawal@gmail.com>
2024-08-06 15:12:37 +0530

    adding rain_water_trapped solution

--- data_struct_algo/array/rain_water_trapped/Code.java
+++ data_struct_algo/array/rain_water_trapped/Code.java
@@ -1,7 +1,21 @@
 package array.rain_water_trapped;
 
+import com.sun.xml.internal.bind.v2.model.core.MaybeElement;
+
 public class Code {
     public int trap(final int[] A) {
-        return 0;
+        int[] left = new int[A.length];
+        int[] right = new int[A.length];
+        left[0] = 0;
+        right[A.length - 1] = 0;
+        int total_water = 0;
+        for (int i = 1; i < A.length; i++) {
+            left[i] = Math.max(left[i-1], A[i-1]);
+            right[A.length - i - 1] = Math.max(right[A.length - i], A[A.length - i]);
+        }
+        for (int i = 0; i < A.length; i++) {
+            total_water += Math.max(Math.min(left[i], right[i]) - A[i], 0);
+        }
+        return total_water;
     }
 }
