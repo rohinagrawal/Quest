2999d7717a5be4a060478a4ee5364da152aeebd7
Rohin Agrawal <rohin.k.agrawal@gmail.com>
2024-08-16 14:25:34 +0530

    adding main

---
+++ data_struct_algo/Main.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Quest, Get Solutions to a collection of Data Structures and Algorithms (DSA) problems.");
+    }
+}
