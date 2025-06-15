4e34d397cc72ccbc404ac0a6c5f1e409ee09b030
Rohin Agrawal <39937951+rohinagrawal@users.noreply.github.com>
2025-06-15 10:20:22 +0530

    Atlassian Question

---
+++ data_struct_algo/hash_map/scrambled_word_from_array/Code.java
@@ -0,0 +1,23 @@
+public class Code {
+    public void unscramble() {
+    String[] words = {"baby", "referee", "cat", "dada", "dog", "bird", "ax", "baz"};
+    String note1 = "ctay";
+    String note2 = "bcanihjsrrrferet";
+    String note3 = "tbaykkjlga";
+    String note4 = "bbbblkkjbaby";
+    String note5 = "dad";
+    String note6 = "breadmaking";
+    String note7 = "dadaa";
+    System.out.println(find(words, note1));
+    System.out.println(find(words, note2));
+    System.out.println(find(words, note3));
+    System.out.println(find(words, note4));
+    System.out.println(find(words, note5));
+    System.out.println(find(words, note6));
+    System.out.println(find(words, note7));
+  }
+  
+  public static String find(String[] words, String note){
+    
+  }
+}
