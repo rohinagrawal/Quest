612beac22b93e1bda15fb49a2ba4d0433754bfe9
Rohin Agrawal <rohin.k.agrawal@gmail.com>
2026-05-17 18:02:56 +0530

    Added Bitmask Range Flip and Count Solution

--- src/main/data_struct_algo/segment_tree/bitmask/Code.cpp
+++ src/main/data_struct_algo/segment_tree/bitmask/Code.cpp
@@ -12,6 +12,85 @@ public:
      * @return           sum of all type-2 query results modulo MOD
      */
     int sumSetBitQueries(int n, const vector<vector<int>>& operations) {
-        return 0;
+        vector<int> bitmask(n, 0);
+        vector<int> segmentTree(4*n, 0);
+        create(bitmask, segmentTree, 0, bitmask.size()-1, 0);
+        int sum = 0;
+        for (int i = 0; i<operations.size(); i++) {
+            if (operations[i][0] == 1) {
+                //Update
+                for (int j = operations[i][1]; j <= operations[i][2]; j++) {
+                    update(bitmask, j, 1-bitmask[j], segmentTree, 0, bitmask.size()-1, 0);
+                }
+            } else {
+                //Query
+                sum+=(query(operations[i][1], operations[i][2], segmentTree, 0, bitmask.size()-1, 0)%MOD);
+                sum%=MOD;
+            }
+        }
+        return sum;
     }
+
+    void create(vector<int> &arr, vector<int> &segmentTree, int rangeStart, int rangeEnd, int treeIndex) {
+        if (rangeStart == rangeEnd) {
+            segmentTree[treeIndex] = arr[rangeStart];
+            return;
+        }
+        int mid = (rangeEnd + rangeStart)/2;
+        create(arr, segmentTree, rangeStart, mid, 2*treeIndex+1);
+        create(arr, segmentTree, mid+1, rangeEnd, 2*treeIndex+2);
+        segmentTree[treeIndex] = merge(segmentTree[2*treeIndex+1], segmentTree[2*treeIndex+2]);
+        return;
+    }
+
+    int merge(int leftNode, int rightNode) {
+        return leftNode + rightNode;
+    }
+
+    int query(int queryLeft, int queryRight, vector<int> &segmentTree, int rangeStart, int rangeEnd, int treeIndex) {
+        if (rangeEnd < queryLeft || rangeStart > queryRight) {
+            return 0;
+        } else if (queryLeft <= rangeStart && queryRight >= rangeEnd) {
+            return segmentTree[treeIndex];
+        } else {
+            int mid = (rangeEnd + rangeStart)/2;
+            int left = query(queryLeft, queryRight, segmentTree, rangeStart, mid, 2*treeIndex+1);
+            int right = query(queryLeft, queryRight, segmentTree, mid+1, rangeEnd, 2*treeIndex+2);
+            return merge(left, right);
+        }
+    }
+
+    void update(vector<int> &arr, int updateIndex, int updateValue, vector<int> &segmentTree, int rangeStart, int rangeEnd, int treeIndex) {
+        if (rangeStart == rangeEnd) {
+            segmentTree[treeIndex] = updateValue;
+            arr[updateIndex] = updateValue;
+            return;
+        }
+        int mid = (rangeEnd + rangeStart)/2;
+        if (updateIndex<=mid) {
+            update(arr, updateIndex, updateValue, segmentTree, rangeStart, mid, 2*treeIndex+1);
+        } else {
+            update(arr, updateIndex, updateValue, segmentTree, mid+1, rangeEnd, 2*treeIndex+2);
+        }
+        segmentTree[treeIndex] = merge(segmentTree[2*treeIndex+1], segmentTree[2*treeIndex+2]);
+        return;
+    }
+
+    /*void update_v2(vector<int> &arr, int updateLeft, int updateRight, vector<int> &segmentTree, int rangeStart, int rangeEnd, int treeIndex) {
+        if (rangeEnd < updateLeft || rangeStart > updateRight) {
+            return;
+        } else {
+            if (rangeStart == rangeEnd) {
+                int updateValue = 1-arr[rangeStart];
+                segmentTree[treeIndex] = updateValue;
+                arr[rangeStart] = updateValue;
+                return;
+            }
+            int mid = (rangeEnd + rangeStart)/2;
+            update(arr, updateLeft, updateRight, segmentTree, rangeStart, mid, 2*treeIndex+1);
+            update(arr, updateLeft, updateRight, segmentTree, mid+1, rangeEnd, 2*treeIndex+2);
+            segmentTree[treeIndex] = merge(segmentTree[2*treeIndex+1], segmentTree[2*treeIndex+2]);
+            return;
+        }
+    }*/
 };
