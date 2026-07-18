8e9306d9328d11db0a773698b2af7f6757c0e2c4
Rohin Agrawal <rohin.k.agrawal@gmail.com>
2026-07-19 01:51:48 +0530

    Added Dijikstras Solution

--- src/main/data_struct_algo/graph/dijikstras/Code.cpp
+++ src/main/data_struct_algo/graph/dijikstras/Code.cpp
@@ -5,4 +5,31 @@ using namespace std;
 class Code {
 public:
 
+    vector<int> dijikstras_heap(vector<vector<pair<int,int>>> &adj, int source) {
+        vector<int> distance (adj.size(), INT_MAX);
+        vector<bool> visited(adj.size(), false);
+        auto comp = [](const pair<int,int> &a, const pair<int,int> &b) {
+            return a.second > b.second;
+        };
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(comp);
+        pq.push({source, 0});
+        while (!pq.empty()) {
+            pair<int, int> temp = pq.top();
+            pq.pop();
+            if (!visited[temp.first]) {
+                distance[temp.first] = temp.second;
+                for (int i = 0; i< adj[temp.first].size(); ++i) {
+                    if (!visited[adj[temp.first][i].first]) {
+                        pq.push({adj[temp.first][i].first, temp.second+adj[temp.first][i].second});
+                    }
+                }
+                visited[temp.first] = true;
+            }
+        }
+        return distance;
+    }
+
+    vector<int> dijikstras_set(vector<vector<int>> &adj, int source) {
+
+    }
 };
