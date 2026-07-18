e0a78f4dcfac61a3148dc95bafd3801686fba5b1
Rohin Agrawal <rohin.k.agrawal@gmail.com>
2026-07-19 02:52:57 +0530

    Added Floyd Warshall Solution

--- src/main/data_struct_algo/graph/floyd_warshall/Code.cpp
+++ src/main/data_struct_algo/graph/floyd_warshall/Code.cpp
@@ -4,5 +4,28 @@ using namespace std;
 
 class Code {
 public:
+    vector<vector<int>> floydWarshall(vector<vector<pair<int,int>>> &adj) {
+        vector<vector<int>> distance(adj.size(), vector<int>(adj.size(), INT_MAX));
 
+        for (int i = 0; i<adj.size(); ++i) {
+            distance[i][i] = 0;
+        }
+
+        for (int i = 0; i<adj.size(); ++i) {
+            for (int j = 0; j<adj[i].size(); ++j) {
+                distance[i][adj[i][j].first] = adj[i][j].second;
+            }
+        }
+
+        for (int k=0; k<distance.size(); ++k) {
+            for (int i = 0; i<distance.size(); ++i) {
+                for (int j = 0; j<distance[i].size(); ++j) {
+                    if (distance[i][k] != INT_MAX && distance[k][j] != INT_MAX) {
+                        distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j]);
+                    }
+                }
+            }
+        }
+        return distance;
+    }
 };
