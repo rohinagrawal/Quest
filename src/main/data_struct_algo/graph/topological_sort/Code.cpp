e4deede7145806d7530ab5ccd0f3d5f9a624e642
Rohin Agrawal <rohin.k.agrawal@gmail.com>
2026-08-11 06:31:27 +0530

    Added topo sort incomplete solution

--- src/main/data_struct_algo/graph/topological_sort/Code.cpp
+++ src/main/data_struct_algo/graph/topological_sort/Code.cpp
@@ -4,5 +4,31 @@ using namespace std;
 
 class Code {
 public:
-
+    vector<int> topologicalOrder(vector<vector<int>> &adj) {
+        vector<int> order;
+        vector<int> inDegree(adj.size(), 0);
+        for (int i = 0; i<adj.size(); ++i) {
+            for (int j = 0; j<adj[i].size(); ++j) {
+                inDegree[adj[i][j]]++;
+            }
+        }
+        queue<int> dfs;
+        for (int i=0; i<inDegree.size(); ++i) {
+            if (inDegree[i]==0) {
+                dfs.push(i);
+            }
+        }
+        while (!dfs.empty()) {
+            int temp = dfs.front();
+            dfs.pop();
+            order.push_back(temp);
+            for (int j = 0; j<adj[temp].size(); ++j) {
+                inDegree[adj[temp][j]]--;
+                if (inDegree[adj[temp][j]]==0) {
+                    dfs.push(adj[temp][j]);
+                }
+            }
+        }
+        return order;
+    }
 };
